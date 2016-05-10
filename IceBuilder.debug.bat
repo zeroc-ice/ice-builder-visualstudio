b1150a718252c5a153737e195a0014d8cdfc1755
Jose <jose@zeroc.com>
2016-05-10 12:20:39 +0200

    Add helper sript to set the debug env

---
+++ IceBuilder.debug.bat
@@ -0,0 +1,10 @@
+
+set ICEBUILDERINSTALLDIR=%LOCALAPPDATA%\Microsoft\VisualStudio\14.0Exp\Extensions\ZeroC\Ice Builder\4.3.2
+
+set IceBuilderCppProps=%ICEBUILDERINSTALLDIR%\Resources\IceBuilder.Cpp.props
+set IceBuilderCppTargets=%ICEBUILDERINSTALLDIR%\Resources\IceBuilder.Cpp.targets
+
+set IceBuilderCsharpProps=%ICEBUILDERINSTALLDIR%\Resources\IceBuilder.Csharp.props
+set IceBuilderCsharpTargets=%ICEBUILDERINSTALLDIR%\Resources\IceBuilder.Csharp.targets
+
+"%VSINSTALLDIR%\Common7\IDE\devenv.exe" /RootSuffix Exp
