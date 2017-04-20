d5499b8a2712767d97d946e07adb7e1958724c96
Jose <jose@zeroc.com>
2017-04-20 16:54:30 +0200

    Visual Studio 2010 support
    This is a strip down version of the builder that allows to build
    Ice and Ice Applications with VS 2010.

--- IceBuilder/Properties/AssemblyInfo.cs
+++ IceBuilder/Properties/AssemblyInfo.cs
@@ -10,10 +10,10 @@ using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("IceBuilder")]
+[assembly: AssemblyTitle("IceBuilderVS2010")]
 [assembly: AssemblyDescription("Ice Builder Visual Studio Extension")]
 [assembly: AssemblyCompany("ZeroC, Inc.")]
 [assembly: AssemblyProduct("Ice Builder")]
 [assembly: AssemblyCopyright("Copyright (c) 2009-2016 ZeroC, Inc.")]
-[assembly: AssemblyVersion("4.3.6")]
+[assembly: AssemblyVersion("4.3.6.0")]
 [assembly: ComVisibleAttribute(false)]
